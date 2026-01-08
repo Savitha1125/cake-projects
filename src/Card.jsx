8156c74d77cec4b25883385cb702f5961973c1fe
Krishna <keppy8520@gmail.com>
2026-01-08 15:55:43 +0530

    final code update

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./card.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import img from "./assets/photo-cake.avif";
 import img1 from "./assets/pinata-cake.avif";
@@ -10,80 +10,56 @@ import img3 from "./assets/kids.avif";
 
 export default function Card() {
   return (
-    <>
-      {/* Heading */}
-      <div className='cake text-center mt-5'>
-        <h2 className='fw-bold'>Cakes</h2>
-        <h3 className='fw-normal dreamy'>Dreamy cakes for every occasion</h3>
-        {/* View All Button */}
-        <Link to="/cakes" className='text-decoration-none text-dark float-end mb-5 button'
-          style={{
-          position: "absolute",
-          right: "50px",
-          transform: "translateY(-80%)",
-        }}>
-         <h5 style={{ backgroundColor: "#e1e0e0ff",fontWeight:"bold",height:"40px",padding:"7px",borderRadius:"5px"}}>VIEW ALL</h5>
-        </Link>
-      </div>
+    <div className="container-fluid px-5 mt-5">
 
-      {/* Card Section */}
-      <div className="container mt-4">
-        <div className="row justify-content-center g-5">
+      {/* HEADING */}
+      <div className="position-relative text-center mb-4">
+        <h2 className="fw-bold">Cakes</h2>
+        <h5 className="fw-normal dreamy">Dreamy cakes for every occasion</h5>
 
-          {/* Card 1 */}
-          <div className="col-lg-3 col-md-6 col-sm-6 col-12 col-xl-3 px-md-4 px-lg-3">
-            <Link to="/photo-cakes" className="text-decoration-none">
-              <div className="card cake-card">
-                <img src={img} alt="Photo Cakes" className="cake-img img-fluid mx-auto d-block" />
-                <div className="card-body">
-                  <h6 className="card-title text-center fw-bold">Photo Cakes</h6>
-                  <p className="card-text text-center">Starting From SGD 13.22</p>
-                </div>
-              </div>
-            </Link>
+        {/* View All Button */} 
+        <Link to="/cakes" className='text-decoration-none text-dark float-end mb-5 button' 
+        style={{ position: "absolute", right: "3px", transform: "translateY(-80%)", }}> 
+        <h5 style={{ backgroundColor: "#e1e0e0ff",fontWeight:"bold",height:"40px",
+        padding:"7px",borderRadius:"5px"}}>
+        VIEW ALL</h5> 
+        </Link> </div>
+      {/* CARDS: FLEX ROW */}
+      <div className="card-row ms-3">
+
+        <div className="card card-custom">
+          <img src={img} className="card-img-top" alt="Photo Cakes"/>
+          <div className="card-body text-center">
+            <h6 className="fw-semibold mb-1">Photo Cakes</h6>
+            <p className="text-muted mb-0">Starting From SGD 12.77</p>
           </div>
-
-          {/* Card 2 */}
-          <div className="col-lg-3 col-md-6 col-sm-6 col-12 col-xl-3 px-md-4 px-lg-3">
-            <Link to="/pinata-cakes" className="text-decoration-none">
-              <div className="card cake-card">
-                <img src={img1} alt="Pinata Cakes" className="cake-img img-fluid mx-auto d-block" />
-                <div className="card-body">
-                  <h6 className="card-title text-center fw-bold">Pinata Cakes</h6>
-                  <p className="card-text text-center">Starting From SGD 20.50</p>
-                </div>
-              </div>
-            </Link>
-          </div>
-
-          {/* Card 3 */}
-          <div className="col-lg-3 col-md-6 col-sm-6 col-12 col-xl-3 px-md-4 px-lg-3">
-            <Link to="/pull-me-up-cakes" className="text-decoration-none">
-              <div className="card cake-card">
-                <img src={img2} alt="Pull Me Up Cakes" className="cake-img img-fluid mx-auto d-block" />
-                <div className="card-body">
-                  <h6 className="card-title text-center fw-bold">Pull Me Up Cakes</h6>
-                  <p className="card-text text-center">Starting From SGD 22.77</p>
-                </div>
-              </div>
-            </Link>
-          </div>
-
-          {/* Card 4 */}
-          <div className="col-lg-3 col-md-6 col-sm-6 col-12 col-xl-3 px-md-4 px-lg-3">
-            <Link to="/kids-cakes" className="text-decoration-none">
-              <div className="card cake-card">
-                <img src={img3} alt="Kids Cakes" className="cake-img img-fluid mx-auto d-block" />
-                <div className="card-body">
-                  <h6 className="card-title text-center fw-bold">Kids Cakes</h6>
-                  <p className="card-text text-center">Starting From SGD 11.01</p>
-                </div>
-              </div>
-            </Link>
-          </div>
-
         </div>
+
+        <div className="card card-custom">
+          <img src={img1} className="card-img-top" alt="Pinata Cakes"/>
+          <div className="card-body text-center">
+            <h6 className="fw-semibold mb-1">Pinata Cakes</h6>
+            <p className="text-muted mb-0">Starting From SGD 19.87</p>
+          </div>
+        </div>
+
+        <div className="card card-custom">
+          <img src={img2} className="card-img-top" alt="Pull Me Up Cakes"/>
+          <div className="card-body text-center">
+            <h6 className="fw-semibold mb-1">Pull Me Up Cakes</h6>
+            <p className="text-muted mb-0">Starting From SGD 22.00</p>
+          </div>
+        </div>
+
+        <div className="card card-custom">
+          <img src={img3} className="card-img-top" alt="Kids Cakes"/>
+          <div className="card-body text-center">
+            <h6 className="fw-semibold mb-1">Kids Cakes</h6>
+            <p className="text-muted mb-0">Starting From SGD 10.64</p>
+          </div>
+        </div>
+
       </div>
-    </>
+    </div>
   );
 }
--- src/Flower.jsx
+++ src/Flower.jsx
@@ -1,106 +1,78 @@
-import React from 'react';
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./card.css";
-import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import img from "./assets/images/flowers-first-image.jpg" ;
-import img1 from "./assets/images/basket-arrangement.avif";
-import img2 from "./assets/images/premium-flowers.webp";
+import img from "./assets/images/flowers-first-image.jpg" ; 
+import img1 from "./assets/images/basket-arrangement.avif"; 
+import img2 from "./assets/images/premium-flowers.webp"; 
 import img3 from "./assets/images/lillies.webp";
 import img4 from "./assets/images/banner-images.webp";
 
-export default function Flower() {
-   const navigate = useNavigate();
-   const handleClick = () => {
-      navigate("/visit-page");  
-  };
+export default function Card() {
   return (
     <>
-      {/* Heading */}
-      <div className='cake text-center mt-5'>
-        <h2 className='fw-bold'>Flowers</h2>
-        <h3 className='fw-normal'>Petals of Happiness</h3>
-        {/* View All Button */}
-        <Link to="/flowers" className='text-decoration-none text-dark float-end mb-5'
-          style={{
-          position: "absolute",
-          right: "60px",
-          transform: "translateY(-80%)",
-        }}>
-         <h5 style={{ backgroundColor: "#e1e0e0ff",fontWeight:"bold",height:"40px",padding:"7px"}}>VIEW ALL</h5>
-        </Link>
-      </div>
+    <div className="container-fluid px-5 mt-5">
 
-      {/* Card Section */}
-      <div className="container mt-4">
-        <div className="row justify-content-center g-4">
+      {/* HEADING */}
+      <div className="position-relative text-center mb-4">
+        <h2 className="fw-bold">Flowers</h2>
+        <h5 className="fw-normal dreamy">Petals of Happiness</h5>
 
-          {/* Card 1 */}
-          <div className="col-lg-3 col-md-6 col-sm-6 col-12 col-xl-3 px-md-4 px-lg-3">
-            <Link to="/photo-cakes" className="text-decoration-none">
-              <div className="card cake-card">
-                <img src={img} alt="Photo Cakes" className="cake-img img-fluid mx-auto d-block" />
-                <div className="card-body">
-                  <h6 className="card-title text-center fw-bold">Roses</h6>
-                  <p className="card-text text-center">Starting From SGD 7.29</p>
-                </div>
-              </div>
-            </Link>
+        {/* View All Button */} 
+        <Link to="/cakes" className='text-decoration-none text-dark float-end mb-5 button' 
+        style={{ position: "absolute", right: "3px", transform: "translateY(-80%)", }}> 
+        <h5 style={{ backgroundColor: "#e1e0e0ff",fontWeight:"bold",height:"40px",
+        padding:"7px",borderRadius:"5px"}}>
+        VIEW ALL</h5> 
+        </Link> </div>
+      {/* CARDS: FLEX ROW */}
+      <div className="card-row ms-3">
+
+        <div className="card card-custom">
+          <img src={img} className="card-img-top" alt="Photo Cakes"/>
+          <div className="card-body text-center">
+            <h6 className="fw-semibold mb-1">Roses</h6>
+            <p className="text-muted mb-0">Starting From SGD 7.09</p>
           </div>
-
-          {/* Card 2 */}
-          <div className="col-lg-3 col-md-6 col-sm-6 col-12 col-xl-3 px-md-4 px-lg-3">
-            <Link to="/pinata-cakes" className="text-decoration-none">
-              <div className="card cake-card">
-                <img src={img1} alt="Pinata Cakes" className="cake-img img-fluid mx-auto d-block" />
-                <div className="card-body">
-                  <h6 className="card-title text-center fw-bold">Basket Arrangements</h6>
-                  <p className="card-text text-center">Starting From SGD 15.32</p>
-                </div>
-              </div>
-            </Link>
-          </div>
-
-          {/* Card 3 */}
-          <div className="col-lg-3 col-md-6 col-sm-6 col-12 col-xl-3 px-md-4 px-lg-3">
-            <Link to="/pull-me-up-cakes" className="text-decoration-none">
-              <div className="card cake-card">
-                <img src={img2} alt="Pull Me Up Cakes" className="cake-img img-fluid mx-auto d-block" />
-                <div className="card-body">
-                  <h6 className="card-title text-center fw-bold">Premium Flowers</h6>
-                  <p className="card-text text-center">Starting From SGD 12.40</p>
-                </div>
-              </div>
-            </Link>
-          </div>
-
-          {/* Card 4 */}
-          <div className="col-lg-3 col-md-6 col-sm-6 col-12 col-xl-3 px-md-4 px-lg-3">
-            <Link to="/kids-cakes" className="text-decoration-none">
-              <div className="card cake-card">
-                <img src={img3} alt="Kids Cakes" className="cake-img img-fluid mx-auto d-block" />
-                <div className="card-body">
-                  <h6 className="card-title text-center fw-bold">Lilies</h6>
-                  <p className="card-text text-center">Starting From SGD 15.32</p>
-                </div>
-              </div>
-            </Link>
-          </div>
-
         </div>
+
+        <div className="card card-custom">
+          <img src={img1} className="card-img-top" alt="Pinata Cakes"/>
+          <div className="card-body text-center">
+            <h6 className="fw-semibold mb-1">Basket  Arrangements</h6>
+            <p className="text-muted mb-0">Starting From SGD 14.90</p>
+          </div>
+        </div>
+
+        <div className="card card-custom">
+          <img src={img2} className="card-img-top" alt="Pull Me Up Cakes"/>
+          <div className="card-body text-center">
+            <h6 className="fw-semibold mb-1">Premium Flowers</h6>
+            <p className="text-muted mb-0">Starting From SGD 12.06</p>
+          </div>
+        </div>
+
+        <div className="card card-custom">
+          <img src={img3} className="card-img-top" alt="Kids Cakes"/>
+          <div className="card-body text-center">
+            <h6 className="fw-semibold mb-1">Lilies</h6>
+            <p className="text-muted mb-0">Starting From SGD 14.90</p>
+          </div>
+        </div>
+
       </div>
-      <div className='container mt-5'>
+    </div>
+     {/* Banner */}
+      <div className="container mt-5">
         <img
           src={img4}
-          className="cash"
-          alt="Cash"
-          onClick={handleClick}
-          style={{
-          cursor: "pointer",
-          }}
+          alt="Banner"
+          className="img-fluid"
+          style={{ cursor: "pointer" }}
+          onClick={() => navigate("/visit-page")}
         />
       </div>
-    </>
+  </>
   );
 }
